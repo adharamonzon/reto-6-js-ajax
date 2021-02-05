fe2bdc797f878232f4c793b3e1d2b6fb2e673d10
adharamonzon <adharamonzon@gmail.com>
2021-02-05 19:53:04 +0100

    done search of cities and states, also shows population

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # reto-6-js-ajax
+
+Este reto tiene como **objetivo** la práctica de peticiones a una API, limpieza de los datos y filtro por nombre.
---
+++ js/main.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+const cities = [];
+let filteredCities = [];
+
+//traer datos de la api
+const getDataFromApi = () => {
+  return fetch(endpoint)
+    .then((response) => response.json())
+    .then((data) => cities.push(...data));
+};
+//spread (...) hace que sea un único array
+
+const findMatches = (wordToMatch, cities) => {
+  return cities.filter(place => {
+    //figured out which city was searched
+    const regex = new RegExp(wordToMatch, 'gi') //aqui matchea la palabra escrita 'gi' = g(global, mira el string entero) i(insensitive, le da igual lowercase que uppercase)
+    return place.city.match(regex) || place.state.match(regex);
+  })
+}
+//pintar el número de la población con comas
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+//pintar la solución de la búsqueda
+function displayMatches () {
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray.map((place) => {
+    const regex = new RegExp(this.value, 'gi');
+    const cityName = place.city.replace(regex, `<mark class="hl">${this.value}</mark>`)
+    const stateName = place.state.replace(regex, `<mark class="hl">${this.value}</mark>`)
+    return `
+    <li>
+    <p class='name'>${cityName}, ${stateName}</p>
+    <p class='population'>${numberWithCommas(place.population)}</p>
+    </li>
+    `
+  }).join('');
+  suggestions.innerHTML = html;
+}
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
+
+getDataFromApi();
